054d71635e7287d6b0ceb8618177f701eecb67ff
Vahan Dağ <vahandag@gmail.com>
2024-06-25 16:11:47 +0300

    main.dart deneme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kan_lazim/core/custom_button.dart';
+import 'package:kan_lazim/screens/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ,
+      home: const Login(),
     );
   }
 }
